64bada8f3925139679998c532f2a94528ee26999
yiyou00@naver.com <yiyou00@naver.com>
2023-04-20 13:19:07 +0900

    [Add] add index css

--- frontend/campus/src/index.tsx
+++ frontend/campus/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
 import router from './router';
-// import './styles/tailwind.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
