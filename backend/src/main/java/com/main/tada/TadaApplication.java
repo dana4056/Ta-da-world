a058337e5d6dbd4a6db01fad6979843a9c6fb952
한원석 <wonsoongnim@naver.com>
2023-04-26 09:42:53 +0900

    [Add] create spring boot project

---
+++ backend/src/main/java/com/main/tada/TadaApplication.java
@@ -0,0 +1,13 @@
+package com.main.tada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/main/tada/TadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.tada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
