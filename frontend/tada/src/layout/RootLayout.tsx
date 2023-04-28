c7553603d9e7f17f034550f0969f5207a74d3380
Ming0Kim <minofficial13@gmail.com>
2023-04-28 13:31:27 +0900

    [Modify] change RootLayout setting

--- frontend/tada/src/layout/RootLayout.tsx
+++ frontend/tada/src/layout/RootLayout.tsx
@@ -4,9 +4,11 @@ import Header from '../components/nav/Header';
 const RootLayout = (): JSX.Element => {
 	const location = useLocation();
 
+	const excludedPaths: string[] = ['/', '/username'];
+
 	return (
 		<div className='flex flex-col items-center w-full h-screen min-h-screen'>
-			{location.pathname !== '/' && <Header />}
+			{!excludedPaths.includes(location.pathname) && <Header />}
 			<main className='w-full h-full'>
 				<Outlet />
 			</main>
