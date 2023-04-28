ad368a9cc5d439f4917ed7f2a7b7f1533acbf092
김민경 <minofficial@naver.com>
2023-04-28 09:56:38 +0900

    Feat fe/mainpage

--- frontend/tada/src/pages/MainPage.tsx
+++ frontend/tada/src/pages/MainPage.tsx
@@ -1,7 +1,57 @@
-function MainPage() : JSX.Element {
+import React, { useState } from 'react';
+
+const logo = require('../assets/images/logo.png');
+
+function MainPage(): JSX.Element {
+	const [activeComponent, setActiveComponent] = useState('User');
+
+	const handleClick = () => {
+		setActiveComponent((defaultComponent) =>
+			defaultComponent === 'User' ? 'Host' : 'User'
+		);
+	};
+
+	const LoginUser = () => (
+		<>
+			<div className='flex flex-col items-center justify-center mb-3 border-b-8 shadow-lg shadow-main bg-white/80 w-72 h-36 rounded-3xl border-b-main3'>
+				<input
+					className='h-10 px-4 mb-5 border shadow-lg placeholder:text-sm placeholder:text-gray2 text-gray5 w-60 rounded-xl border-gray2'
+					type='text'
+					placeholder='참여코드를 입력하세요!'
+				/>
+				<button className='h-10 font-semibold text-white shadow-lg rounded-xl w-60 bg-gradient-to-r from-blue to-blue2'>
+					입장
+				</button>
+			</div>
+			<button
+				type='button'
+				onClick={handleClick}
+				className='text-sm text-white border-b'
+			>
+				<p>{'>'} 호스트는 이쪽으로 입장해주세요</p>
+			</button>
+		</>
+	);
+
+	const LoginHost = () => (
+		<div className='flex flex-col items-center justify-center'>
+			<div className='mb-3 bg-yellow-500 shadow-lg w-72 h-36 rounded-3xl'>
+				카카오로 로그인
+			</div>
+			<button
+				type='button'
+				onClick={handleClick}
+				className='text-sm text-white border-b'
+			>
+				<p>{'>'} 참가자는 이쪽으로 입장해주세요</p>
+			</button>
+		</div>
+	);
+
 	return (
-		<div className="text-red-500">
-      Hello
+		<div className='flex flex-col items-center justify-center h-full'>
+			<img className='mb-5' src={logo} alt='logo' />
+			{activeComponent === 'User' ? <LoginUser /> : <LoginHost />}
 		</div>
 	);
 }
---
+++ frontend/tada/src/pages/UserWaitPage.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+interface User {
+	id: number;
+	name: string;
+	profileImage: string;
+}
+
+const userProfile = require('../assets/images/dummy_userprofile.png');
+
+const UserWaitPage = () => {
+	const user: User = {
+		id: 1,
+		name: '친구많은한원석',
+		profileImage: userProfile,
+	};
+
+	const members: User[] = [
+		{ id: 2, name: '친구없는한원석', profileImage: userProfile },
+		{ id: 3, name: '기침하는한원석', profileImage: userProfile },
+		{ id: 4, name: '재채기쟁이한원석', profileImage: userProfile },
+		{ id: 5, name: '친구없는한원석', profileImage: userProfile },
+		{ id: 6, name: '기침하는한원석', profileImage: userProfile },
+		{ id: 7, name: '재채기쟁이한원석', profileImage: userProfile },
+	];
+
+	return (
+		<div className='w-full h-full bg-white2'>
+			{/* 상단 프로필 단 */}
+			<div className='flex items-center justify-center shadow-lg h-52 bg-main rounded-b-3xl'>
+				<div className='flex items-center justify-center rounded-full w-52 h-52 bg-main2/50'>
+					<div className='w-40 h-40 rounded-full bg-main3/50'>
+						<img className='' src={user.profileImage} alt='pic' />
+						<div className='flex items-center justify-center w-40 h-12 text-lg font-semibold text-white rounded-full shadow-lg bg-gradient-to-r from-blue to-blue2'>
+							<p>{user.name}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='p-8'>
+				<div className='flex items-center'>
+					<p className='mr-3 font-semibold text-gray4'>현재 참가자</p>
+					<p className='px-2 py-1 font-semibold text-center text-white border-white rounded-lg bg-gradient-to-r from-orange to-orange2'>
+						13
+					</p>
+				</div>
+				<div className='px-2 mt-4 space-y-2 overflow-auto h-80'>
+					{members.map((member) => (
+						<div
+							className='flex items-center h-16 pl-3 font-bold bg-white shadow-lg text- rounded-2xl text-main w-ful'
+							key={member.id}
+						>
+							<img
+								className='w-10 h-10 mr-3'
+								src={member.profileImage}
+								alt=''
+							/>
+							<p>{member.name}</p>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UserWaitPage;
