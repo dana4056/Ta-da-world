f808961b80b7c8e775625ec0ce97df3bc285d21c
Ming0Kim <minofficial13@gmail.com>
2023-04-28 13:38:13 +0900

    [Add] create usernamepage

--- frontend/tada/src/pages/UserNamePage.tsx
+++ frontend/tada/src/pages/UserNamePage.tsx
@@ -20,17 +20,19 @@ function UserNamePage(): JSX.Element {
 			<img className='mb-5' src={logo} alt='logo' />
 			<div className='flex flex-col items-center justify-center mb-3 border-b-8 shadow-lg shadow-main bg-white/80 w-72 h-36 rounded-3xl border-b-main3'>
 				<input
-					className='h-10 px-4 mb-5 border shadow-lg placeholder:text-sm placeholder:text-gray2 text-gray5 w-60 rounded-xl border-gray2'
+					className={`h-10 px-4 mb-5 border shadow-lg placeholder:text-sm placeholder:text-gray2 text-gray5 w-60 rounded-xl ${
+						name === '한원석' ? 'border-2 border-red' : 'border-gray2'
+					}`}
 					type='text'
 					placeholder='닉네임을 입력해주세요!'
 					value={name}
 					onChange={handleNameChange}
 				/>
 				<button
-					className={`h-10 font-semibold text-white shadow-lg rounded-xl w-60  ${
+					className={`h-10 text-white shadow-lg rounded-xl w-60  ${
 						name === '한원석'
-							? `bg-red ${styles.shake}`
-							: 'bg-gradient-to-r from-orange to-orange2'
+							? `bg-red text-sm ${styles.shake}`
+							: 'bg-gradient-to-r from-orange to-orange2 font-semibold'
 					}`}
 				>
 					{name === '한원석' ? '이미 사용중인 닉네임입니다' : '확인'}
