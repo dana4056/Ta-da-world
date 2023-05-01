c56e5ed69b7fb6d7d46fa76504173403e940d996
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-05-01 14:51:13 +0900

    [Modify] add context-path /api

--- backend/tada/src/main/java/com/tada/TadaApplication.java
+++ backend/tada/src/main/java/com/tada/TadaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class TadaApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/api");
 		SpringApplication.run(TadaApplication.class, args);
 
 	}
