47a0b9e5e5eab94e4390bb4aa00652681e9fdf2c
dana <dana4056@naver.com>
2023-05-02 10:43:19 +0900

    [Modify] modify swagger response detail

--- backend/tada/src/main/java/com/tada/controller/RoomController.java
+++ backend/tada/src/main/java/com/tada/controller/RoomController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.v3.oas.annotations.Operation;
 
 import org.slf4j.Logger;
@@ -28,11 +31,15 @@ import com.tada.domain.dto.RoomResponse;
 import com.tada.service.RoomService;
 import com.tada.util.JwtTokenProvider;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @CrossOrigin
 @RestController
 @RequiredArgsConstructor
+@Api(tags = "Room Controller")
 @RequestMapping("/rooms")
 public class RoomController {
 
@@ -44,6 +51,10 @@ public class RoomController {
 	@Operation(summary = "방 기본 정보 조회", description = "호스트가 생성한 방의 기본정보 조회<br><br>"
 	+"<b>[PathVariable]</b><br>"
 	+"roomId: 방 고유번호")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "방 기본 정보 조회 성공"),
+		@ApiResponse(responseCode = "500", description = "서버에러")
+	})
 	public ResponseEntity<?> readRoom(@PathVariable Long roomId){
 		RoomResponse response = null;
 		HttpStatus status = HttpStatus.OK;
@@ -60,6 +71,12 @@ public class RoomController {
 
 	@PostMapping("/host")
 	@Operation(summary = "방 생성", description = "호스트가 처음 방 생성 버튼 클릭했을 때 방 생성")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "방 생성 성공"),
+		@ApiResponse(responseCode = "401", description = "액세스 토큰 만료"),
+		@ApiResponse(responseCode = "403", description = "토큰 에러 (토큰 없음)"),
+		@ApiResponse(responseCode = "500", description = "서버에러")
+	})
 	public ResponseEntity<?> createRoom(HttpServletRequest request) {
 		HttpStatus status = HttpStatus.OK;
 		String header = request.getHeader("Authorization");
@@ -89,6 +106,12 @@ public class RoomController {
 	@Operation(summary = "방 정보 수정", description = "호스트가 생성한 방의 기본정보를 입력해 저장/수정<br><br>"
 	+"<b>[PathVariable]</b><br>"
 	+"roomId: 방 고유번호")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "방 정보 수정 성공"),
+		@ApiResponse(responseCode = "401", description = "액세스 토큰 만료"),
+		@ApiResponse(responseCode = "403", description = "토큰 에러 (토큰 없음)"),
+		@ApiResponse(responseCode = "500", description = "서버에러")
+	})
 	public ResponseEntity<?> modifyRoom(HttpServletRequest request, @PathVariable Long roomId, @RequestBody RoomRequest roomRequest) {
 
 		HttpStatus status = HttpStatus.OK;
@@ -121,6 +144,12 @@ public class RoomController {
 		+ "- 3: 게임중 (게임을 하고 있는 상태로, 게임 현황을 볼 수 있음)<br>"
 		+ "- 4: 게임 종료 (게임이 종료되고 게임 결과창이 보이는 상태)<br><br>"
 		+ "상태코드 0과 2로는 변경X / 2로 변경하는 API 별도 존재!!")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "방 상태 변경 성공"),
+		@ApiResponse(responseCode = "401", description = "액세스 토큰 만료"),
+		@ApiResponse(responseCode = "403", description = "토큰 에러 (토큰 없음)"),
+		@ApiResponse(responseCode = "500", description = "서버에러")
+	})
 	public ResponseEntity<?> modifyRoomStatus(HttpServletRequest request, @PathVariable Long roomId, @RequestBody Map<String, Integer> statusRequest){
 		HttpStatus status = HttpStatus.OK;
 		String header = request.getHeader("Authorization");
@@ -150,6 +179,12 @@ public class RoomController {
 		+ "<b>[변경 가능한 상태 목록]</b><br>"
 		+ "- 2: 대기방 (게임을 시작하기 전 상태)<br><br>"
 		+ "*대기방으로 이동할 때는 접속코드 반환해야해서 분리")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "방 상태 대기중으로 변경 성공"),
+		@ApiResponse(responseCode = "401", description = "액세스 토큰 만료"),
+		@ApiResponse(responseCode = "403", description = "토큰 에러 (토큰 없음)"),
+		@ApiResponse(responseCode = "500", description = "서버에러")
+	})
 	public ResponseEntity<?> moveToWaitingRoom(HttpServletRequest request, @PathVariable Long roomId){
 
 		HttpStatus status = HttpStatus.OK;
@@ -186,6 +221,12 @@ public class RoomController {
 		+ "- 0: 방없음 (방이 없는 상태로, 새로운 방 생성 가능)<br>- 1: 방 생성 및 수정 (방을 생성하고 정보를 수정하고 있는 상태로, 대기방 가기전)<br>"
 		+ "- 2: 대기방 (게임을 시작하기 전 상태)<br>- 3: 게임중 (게임을 하고 있는 상태로, 게임 현황을 볼 수 있음)<br>"
 		+ "- 4: 게임 종료 (게임이 종료되고 게임 결과창이 보이는 상태)")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "방 상태 조회 성공"),
+		@ApiResponse(responseCode = "401", description = "액세스 토큰 만료"),
+		@ApiResponse(responseCode = "403", description = "토큰 에러 (토큰 없음)"),
+		@ApiResponse(responseCode = "500", description = "서버에러")
+	})
 	public ResponseEntity<?> readRoomStatus(HttpServletRequest request){
 
 		HttpStatus status = HttpStatus.OK;
