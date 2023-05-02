8ccba6744071ab8622201efc8251881480fc7420
dana <dana4056@naver.com>
2023-05-02 12:59:59 +0900

    [Modify] Handle Exception about User Controller

--- backend/tada/src/main/java/com/tada/controller/RoomController.java
+++ backend/tada/src/main/java/com/tada/controller/RoomController.java
@@ -1,20 +1,16 @@
 package com.tada.controller;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.v3.oas.annotations.Operation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tada.domain.dto.RoomRequest;
@@ -31,7 +26,6 @@ import com.tada.domain.dto.RoomResponse;
 import com.tada.service.RoomService;
 import com.tada.util.JwtTokenProvider;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
--- backend/tada/src/main/java/com/tada/controller/UserController.java
+++ backend/tada/src/main/java/com/tada/controller/UserController.java
@@ -3,6 +3,8 @@ package com.tada.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -27,19 +29,41 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UserService userService;
+	public static final Logger logger = LoggerFactory.getLogger(HostController.class);
 
 	@PostMapping
-	@Operation(summary = "참가자 방 입장", description = "참가자가 참가코드를 통해 특정 게임방에 참가")
+	@Operation(summary = "참가자 방 입장", description = "참가자가 참가코드를 통해 특정 게임방에 참가<br><br>"
+		+"<b>[RequestParam]</b><br>"
+		+"code: 방 참가 코드<br>"
+		+"<b>[RequestBody]</b><br>"
+		+"deviceId: 참가자 디바이스 고유번호?<br>imgNo: 유저가 선택한 캐릭터 번호<br>nick: 참가자 닉네임")
 	public ResponseEntity<?> enterRoom(@RequestParam("code") String code, @RequestBody UserRequest userRequest){
+		HttpStatus status = HttpStatus.OK;
 
-		Map<String, Long> response = userService.saveUser(code, userRequest);
-		return new ResponseEntity<Map<String, Long>>(response, HttpStatus.OK);
+		try{
+			Map<String, Long> response = userService.saveUser(code, userRequest);
+			return new ResponseEntity<Map<String, Long>>(response, status);
+		}catch (Exception e){
+			logger.error("참가자 방 입장 실패 : {}", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			return new ResponseEntity<>(status);
+		}
 	}
 
 	@GetMapping
-	@Operation(summary = "참가자 리스트 조회", description = "특정 게임방 안에 있는 참가자 리스트를 반환")
+	@Operation(summary = "참가자 리스트 조회", description = "특정 게임방 안에 있는 참가자 리스트를 반환"
+		+"<b>[RequestParam]</b><br>"
+		+"room: 방 고유번호")
 	public ResponseEntity<?> readUserList(@RequestParam("room") Long roomId){
-		List<UserResponse> list = userService.readUserList(roomId);
-		return new ResponseEntity<List<UserResponse>>(list, HttpStatus.OK);
+		HttpStatus status = HttpStatus.OK;
+
+		try{
+			List<UserResponse> list = userService.readUserList(roomId);
+			return new ResponseEntity<List<UserResponse>>(list, status);
+		}catch (Exception e) {
+			logger.error("참가자 리스트 조회 실패 : {}", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			return new ResponseEntity<>(status);
+		}
 	}
 }
