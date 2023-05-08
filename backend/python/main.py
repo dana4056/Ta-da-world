e16d1de7fd32db2377d5eec60042e44081eb3664
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-05-08 16:49:42 +0900

    [Modify] backend/python.main.py - set contextPath

--- backend/python/main.py
+++ backend/python/main.py
@@ -5,11 +5,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/infer")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("infer/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
