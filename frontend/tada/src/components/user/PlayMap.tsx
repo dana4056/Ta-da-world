20706b92364263287aa7912547214144e3b099cf
박진우 <zacinthepark@gmail.com>
2023-05-08 15:04:33 +0900

    [feat-fe/play] -> [dev-front]

--- frontend/tada/src/components/user/PlayMap.tsx
+++ frontend/tada/src/components/user/PlayMap.tsx
@@ -1,24 +1,12 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useWatchLocation from '../../hooks/useWatchLocation';
-import { WatchLocation } from '../../util/Interface';
+import { TreasureInfo, WatchLocation } from '../../util/Interface';
 import { getDistanceFromLatLonInKm } from '../../util/Calculate';
 // 리덕스와 연동하여 이미지는 차후 변경해줘야함
 const playerImage = require('../../assets/images/avatar4.jpg');
-const treasureImage = require('../../assets/images/opentreasure.png');
+const treasureImage = require('../../assets/images/treasure_opacity.gif');
 
-interface Treasure {
-	id: number,
-	imgPath: string,
-	lat: string,
-	lng: string,
-	hint: string,
-	rewardImgPath: string,
-	reward: string,
-	status: boolean,
-	finderNick: string
-}
-
-const treasures: Treasure[] = [
+const treasures: TreasureInfo[] = [
 	{
 		id: 1,
 		imgPath: 'string',
@@ -62,17 +50,62 @@ const treasures: Treasure[] = [
 		reward: 'string',
 		status: true,
 		finderNick: 'string'
+	},
+	{
+		id: 5,
+		imgPath: 'string',
+		lat: '37.51254109829138',
+		lng: '127.03182025987998',
+		hint: 'string',
+		rewardImgPath: 'string',
+		reward: 'string',
+		status: true,
+		finderNick: 'string'
+	},
+	{
+		id: 6,
+		imgPath: 'string',
+		lat: '37.51227077723606',
+		lng: '127.03189366540164',
+		hint: 'string',
+		rewardImgPath: 'string',
+		reward: 'string',
+		status: true,
+		finderNick: 'string'
+	},
+	{
+		id: 7,
+		imgPath: 'string',
+		lat: '37.51308608143174',
+		lng: '127.0322785841547',
+		hint: 'string',
+		rewardImgPath: 'string',
+		reward: 'string',
+		status: true,
+		finderNick: 'string'
+	},
+	{
+		id: 8,
+		imgPath: 'string',
+		lat: '37.51357291306831',
+		lng: '127.03120142017693',
+		hint: 'string',
+		rewardImgPath: 'string',
+		reward: 'string',
+		status: true,
+		finderNick: 'string'
 	}
 ];
 
 function PlayMap(): JSX.Element {
 	const playerLocation: WatchLocation = useWatchLocation();
+	const [currentTreasure, setCurrentTreasure] = useState<TreasureInfo | null>(null);
 	// create a marker only if markerRef.current is not initialized
 	// const markerRef = useRef<any | null>(null);
 
 	useEffect(() => {
 		if (playerLocation.data) {
-			console.log('playerLocation: ', playerLocation.data);
+			console.log('new location rendering: ', playerLocation.data);
 			const container = document.getElementById('map');
 			const options = {
 				center: new window.kakao.maps.LatLng(playerLocation.data.latitude, playerLocation.data.longitude),
@@ -88,7 +121,7 @@ function PlayMap(): JSX.Element {
 				position: new window.kakao.maps.LatLng(playerLocation.data.latitude, playerLocation.data.longitude),
 				image: playerMarkerImage
 			});
-			console.log('playerMarker: ', playerMarker);
+			// console.log('playerMarker: ', playerMarker);
 			if (treasures) {
 				// const container = document.getElementById('map');
 				// const options = {
@@ -101,12 +134,23 @@ function PlayMap(): JSX.Element {
 				const treasureMarkerImage = new window.kakao.maps.MarkerImage(treasureImage, imageSize);
 	
 				for (let i = 0; i < treasures.length; i ++) {
-					if (treasures[i].status) {
+					const distance = getDistanceFromLatLonInKm(
+						playerLocation.data.latitude,
+						playerLocation.data.longitude,
+						parseFloat(treasures[i].lat),
+						parseFloat(treasures[i].lng)
+					);
+					if (distance < 0.05) {
 						const marker = new window.kakao.maps.Marker({
 							map: map,
 							position: new window.kakao.maps.LatLng(treasures[i].lat, treasures[i].lng),
 							clickable: true,
 							image: treasureMarkerImage
+						});
+
+						window.kakao.maps.event.addListener(marker, 'click', () => {
+							setCurrentTreasure(treasures[i]);
+							console.log('currentTreasure: ', currentTreasure);
 						});
 					}
 				}
