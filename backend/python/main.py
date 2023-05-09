880c8e280c46265d2b1f735193cbed84518afcc6
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-05-09 17:19:34 +0900

    [Modify] remove router, add path /papi

--- backend/python/main.py
+++ backend/python/main.py
@@ -4,15 +4,11 @@ from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
 
-router = APIRouter()
-
-app.include_router(router, prefix="/papi")
-
-@app.get("/")
+@app.get("/papi")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/papi/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
