6469c1d15c1bed544edab6ad02b415329b344c66
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-05-09 15:24:18 +0900

    [Modify] backend/python/main.py

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,15 +1,18 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI(root_path="/papi")
+app = FastAPI()
 
+router = APIRouter()
 
-@app.get("/papi")
+app.include_router(router, prefix="/papi")
+
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/papi/items/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
