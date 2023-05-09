2398ad4c22fd5709d7221bcd0574996c877a7261
jaeheekim970327 <jaeheekim970327@gmail.com>
2023-05-09 14:16:09 +0900

    [Modify] back/python/main.py

--- backend/python/main.py
+++ backend/python/main.py
@@ -5,11 +5,11 @@ from fastapi import FastAPI
 app = FastAPI(root_path="/papi")
 
 
-@app.get("/")
+@app.get("/papi")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/papi/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
