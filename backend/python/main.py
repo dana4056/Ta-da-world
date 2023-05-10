8de3a6da5facb1769750d6858805dc061e7e7304
dana <dana4056@naver.com>
2023-05-10 16:10:08 +0900

    [Modify] localhost -> 127.0.0.1

--- backend/python/main.py
+++ backend/python/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
     docs_url="/papi/docs",
     openapi_url="/openapi.json",
     servers=[
-        {"url": "http://localhost:8000"},
+        {"url": "http://127.0.0.1:8000"},
         {"url": "https://ta-da.world"}
     ]
 )
@@ -23,8 +23,8 @@ app = FastAPI(
 origins = [
     "http://ta-da.world",
     "https://ta-da.world",
-    "http://localhost",
-    "http://localhost:8080",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
