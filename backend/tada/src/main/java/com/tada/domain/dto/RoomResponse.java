0dc2444d6f033af4dce9be74cdae8da372a38956
dana <dana4056@naver.com>
2023-05-11 16:36:11 +0900

    [Add] Add the number of treasures and participants to the treasure detail view API

--- backend/tada/src/main/java/com/tada/domain/dto/RoomResponse.java
+++ backend/tada/src/main/java/com/tada/domain/dto/RoomResponse.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 
 import com.tada.domain.entity.Room;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,11 +17,20 @@ public class RoomResponse {
 	private String name;
 	private Long playTime;
 	private LocalDateTime startTime;
+	private Long playerCnt;
+	private Long treasureCnt;
 
 	public RoomResponse(Room room){
 		this.id = room.getId();
 		this.name = room.getName();
 		this.playTime = room.getPlayTime();
 		this.startTime = room.getStartTime();
+	}
+
+	public void updatePlayerCnt(Long cnt){
+		this.playerCnt = cnt;
+	}
+	public void updateTreasureCnt(Long cnt){
+		this.treasureCnt = cnt;
 	}
 }
