41ab6339831124eae296143a85c716310f6e7397
이유경 <yiyou00@naver.com>
2023-05-12 16:21:06 +0900

    [Feat fe/serverhost] -> [dev-front]

--- frontend/tada/src/components/hosthome/DeleteHost.tsx
+++ frontend/tada/src/components/hosthome/DeleteHost.tsx
@@ -1,7 +1,48 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import useApi from '../../hooks/useApi';
+import useLogout from '../../hooks/useLogout';
+
 function DeleteHost(): JSX.Element {
+	const deleteApi = useApi();
+	const logoutApi = useLogout();
+
+	useEffect(()=>{
+		if(deleteApi.data?.success){
+			logoutApi.handleLogout();
+		} else if(deleteApi.data){
+			Swal.fire({
+				icon: 'warning',               
+				width: 300,
+				iconColor: '#2BDCDB',
+				text: '회원탈퇴 실패!', 
+				confirmButtonColor: '#2BDCDB',
+				confirmButtonText: '확인',
+			});
+		}
+	}, [deleteApi.data]);
+
+	const deleteHost = () :void => {
+		Swal.fire({
+			text: '회원 탈퇴를 진행하시겠습니까?',
+			width: 300,
+			showCancelButton: true,
+			iconColor: '#2BDCDB ',
+			confirmButtonColor: '#2BDCDB ',
+			confirmButtonText: '탈퇴',
+			cancelButtonText: '취소'
+		}).then(function(e){
+			if(e.isConfirmed === true) {
+				//방 변경
+				deleteApi.fetchNotBodyApiWithToken('DELETE', '/users');
+			}
+		});
+	};
+	
 	return (
 		<div className="w-full flex justify-end items-end text-white mt-11 pr-4">
-      회원탈퇴
+     		<p onClick={deleteHost}>회원탈퇴</p>
 		</div>
 	);
 }
